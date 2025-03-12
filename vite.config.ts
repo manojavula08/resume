aee84da7f0f2817a304d54757e3aa7baf3567b75
manojavula08 <manojreddy.avulamr@gmail.com>
2025-03-12 11:12:03 -0400

    FitHub_Pages

--- vite.config.ts
+++ vite.config.ts
@@ -19,5 +19,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/resume/",
+  base:  mode === "development" ? "/" : "/resume/",
 }));
